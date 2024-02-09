36683d15c3421bb5fce0037acf766885158af57c
Theesan <mohamedtheesan4@gmail.com>
2024-02-09 11:06:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dbConnct = require('./dbConnect');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+dbConnct();
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
---
+++ dbConnect.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const DB_URL = "mongodb+srv://myAtlasDBUser:12345@myatlasclusteredu.zdyf7zp.mongodb.net/wikiDB";
+
+async function dbConnect() {
+  await mongoose
+    .connect(
+        DB_URL
+    )
+    .then(() => {
+      console.log("Successfully connected to MongoDB Atlas!");
+    })
+    .catch((error) => {
+      console.log("Unable to connect to MongoDB Atlas!");
+      console.error(error);
+    });
+}
+
+module.exports = dbConnect;
